133435283e2fb36af42eae003fa5af225df61da3
chiefexb <chief_exb@mail.ru>
2021-04-17 00:54:27 +0300

    first commit

---
+++ circle.php
@@ -0,0 +1,14 @@
+
+<?php
+class Figure
+{
+    // объявление свойства
+    public $var = 'значение по умолчанию';
+
+    // объявление метода
+    public function displayVar() {
+        echo $this->var;
+    }
+}
+?>
+
